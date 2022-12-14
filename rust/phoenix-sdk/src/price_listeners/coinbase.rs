afb8183befb642806b959e2aa4359855a6c85a9c
Jarry Xiao <jarry.xiao@gmail.com>
2022-12-14 13:17:16 -0600

    refactor loop to break instead of continue

--- rust/phoenix-sdk/src/price_listeners/coinbase.rs
+++ rust/phoenix-sdk/src/price_listeners/coinbase.rs
@@ -1,5 +1,6 @@
 use crate::{market_event_handler::SDKMarketEvent, orderbook::Orderbook};
 use coinbase_pro_rs::structs::reqs::OrderSide;
+use coinbase_pro_rs::wsfeed::{CBSink, CBStream};
 use coinbase_pro_rs::{structs::wsfeed::*, WSFeed};
 use futures::StreamExt;
 use phoenix_types::enums::*;
@@ -79,148 +80,153 @@ impl CoinbasePriceListener {
                 ))
                 .unwrap();
 
-            loop {
-                let msg = rt.block_on(stream.next());
-                if let Some(Ok(msg)) = msg {
-                    match msg {
-                        Message::Level2(level2) => match level2 {
-                            Level2::Snapshot { asks, bids, .. } => {
-                                let mut modified_ladder = ladder.write().unwrap();
-                                let mut response_ok = true;
-                                let update_bids = bids
-                                    .iter()
-                                    .filter_map(|bid| {
-                                        if bid.price.is_nan()
-                                            || bid.price.is_infinite()
-                                            || bid.price <= 0.0
-                                        {
-                                            response_ok = false;
-                                            None
-                                        } else {
-                                            Some((
-                                                Decimal::from_f64(bid.price).map_or_else(
-                                                    || {
-                                                        response_ok = false;
-                                                        None
-                                                    },
-                                                    |price| Some(price),
-                                                )?,
-                                                bid.size,
-                                            ))
-                                        }
-                                    })
-                                    .collect::<Vec<_>>();
+            Self::run_listener(&rt, &mut stream, ladder.clone(), sender.clone());
 
-                                modified_ladder.update_orders(Side::Bid, update_bids);
+            thread::sleep(std::time::Duration::from_secs(10));
+        }
+    }
 
-                                let update_asks = asks
-                                    .iter()
-                                    .filter_map(|ask| {
-                                        if ask.price.is_nan()
-                                            || ask.price.is_infinite()
-                                            || ask.price <= 0.0
-                                        {
-                                            response_ok = false;
-                                            None
-                                        } else {
-                                            Some((
-                                                Decimal::from_f64(ask.price).map_or_else(
-                                                    || {
-                                                        response_ok = false;
-                                                        None
-                                                    },
-                                                    |price| Some(price),
-                                                )?,
-                                                ask.size,
-                                            ))
-                                        }
-                                    })
-                                    .collect::<Vec<_>>();
-
-                                modified_ladder.update_orders(Side::Ask, update_asks);
-                                if !response_ok {
-                                    println!(
-                                        "Response is invalid, bids: {:?}, asks {:?}",
-                                        bids, asks
-                                    );
-                                    continue;
+    fn run_listener(
+        rt: &tokio::runtime::Runtime,
+        stream: &mut (impl CBStream + CBSink),
+        ladder: Arc<RwLock<Orderbook<Decimal, f64>>>,
+        sender: Sender<Vec<SDKMarketEvent>>,
+    ) {
+        loop {
+            let event = rt.block_on(stream.next());
+            let msg = if let Some(Ok(msg)) = event {
+                msg
+            } else {
+                println!(
+                    "Issue retrieving next message from Coinbase WS: {:?}",
+                    event
+                );
+                println!("Disconnecting for 5 seconds then reconnecting to Coinbase WS");
+                break;
+            };
+            match msg {
+                Message::Level2(level2) => match level2 {
+                    Level2::Snapshot { asks, bids, .. } => {
+                        let mut modified_ladder = ladder.write().unwrap();
+                        let mut response_ok = true;
+                        let update_bids = bids
+                            .iter()
+                            .filter_map(|bid| {
+                                if bid.price.is_nan() || bid.price.is_infinite() || bid.price <= 0.0
+                                {
+                                    response_ok = false;
+                                    None
+                                } else {
+                                    Some((
+                                        Decimal::from_f64(bid.price).map_or_else(
+                                            || {
+                                                response_ok = false;
+                                                None
+                                            },
+                                            |price| Some(price),
+                                        )?,
+                                        bid.size,
+                                    ))
                                 }
-                            }
-                            Level2::L2update { changes, .. } => {
-                                let mut modified_ladder = ladder.write().unwrap();
-                                for change in changes {
-                                    if change.price.is_nan()
-                                        || change.price.is_infinite()
-                                        || change.price <= 0.0
-                                    {
-                                        println!("Invalid price: {:?}", change.price);
-                                        continue;
-                                    }
-                                    let decimal_price = match Decimal::from_f64(change.price) {
-                                        None => {
-                                            println!("Invalid price: {:?}", change.price);
-                                            continue;
-                                        }
-                                        Some(p) => p,
-                                    };
-                                    match change.side {
-                                        OrderSide::Buy => {
-                                            modified_ladder.update_orders(
-                                                Side::Bid,
-                                                vec![(decimal_price, change.size)],
-                                            );
-                                        }
-                                        OrderSide::Sell => {
-                                            modified_ladder.update_orders(
-                                                Side::Ask,
-                                                vec![(decimal_price, change.size)],
-                                            );
-                                        }
-                                    }
+                            })
+                            .collect::<Vec<_>>();
+
+                        modified_ladder.update_orders(Side::Bid, update_bids);
+
+                        let update_asks = asks
+                            .iter()
+                            .filter_map(|ask| {
+                                if ask.price.is_nan() || ask.price.is_infinite() || ask.price <= 0.0
+                                {
+                                    response_ok = false;
+                                    None
+                                } else {
+                                    Some((
+                                        Decimal::from_f64(ask.price).map_or_else(
+                                            || {
+                                                response_ok = false;
+                                                None
+                                            },
+                                            |price| Some(price),
+                                        )?,
+                                        ask.size,
+                                    ))
                                 }
-                            }
-                        },
-                        Message::Ticker(ticker) => {
-                            let price = ticker.price();
-                            if price.is_nan() || price.is_infinite() || *price <= 0.0 {
-                                println!(
-                                    "Price is invalid: {}, reconnecting as after 10 seconds",
-                                    price
-                                );
+                            })
+                            .collect::<Vec<_>>();
+
+                        modified_ladder.update_orders(Side::Ask, update_asks);
+                        if !response_ok {
+                            println!("Response is invalid, bids: {:?}, asks {:?}", bids, asks);
+                            break;
+                        }
+                    }
+                    Level2::L2update { changes, .. } => {
+                        let mut modified_ladder = ladder.write().unwrap();
+                        for change in changes {
+                            if change.price.is_nan()
+                                || change.price.is_infinite()
+                                || change.price <= 0.0
+                            {
+                                println!("Invalid price: {:?}", change.price);
                                 break;
                             }
-                            match sender
-                                .send(vec![SDKMarketEvent::FairPriceUpdate { price: *price }])
-                            {
-                                Ok(_) => {}
-                                Err(e) => println!("Error while sending fair price update: {}", e),
+                            let decimal_price = match Decimal::from_f64(change.price) {
+                                None => {
+                                    println!("Invalid price: {:?}", change.price);
+                                    break;
+                                }
+                                Some(p) => p,
+                            };
+                            match change.side {
+                                OrderSide::Buy => {
+                                    modified_ladder.update_orders(
+                                        Side::Bid,
+                                        vec![(decimal_price, change.size)],
+                                    );
+                                }
+                                OrderSide::Sell => {
+                                    modified_ladder.update_orders(
+                                        Side::Ask,
+                                        vec![(decimal_price, change.size)],
+                                    );
+                                }
                             }
-                            continue;
                         }
-                        Message::Error { message } => println!("Error: {}", message),
-                        Message::InternalError(_) => panic!("internal_error"),
-                        other => println!("{:?}", other),
-                    };
-
-                    let vwap = ladder.read().unwrap().vwap(3);
-                    if vwap.is_nan() || vwap.is_infinite() || vwap <= 0.0 {
+                    }
+                },
+                Message::Ticker(ticker) => {
+                    let price = ticker.price();
+                    if price.is_nan() || price.is_infinite() || *price <= 0.0 {
                         println!(
                             "Price is invalid: {}, reconnecting as after 10 seconds",
-                            vwap
+                            price
                         );
-                        break;
+                        return;
                     }
-                    match sender.send(vec![SDKMarketEvent::FairPriceUpdate { price: vwap }]) {
+                    match sender.send(vec![SDKMarketEvent::FairPriceUpdate { price: *price }]) {
                         Ok(_) => {}
-                        Err(e) => println!("Error while sending vwap update: {}", e),
+                        Err(e) => println!("Error while sending fair price update: {}", e),
                     }
-                } else {
-                    println!("Issue retrieving next message from Coinbase WS: {:?}", msg);
-                    println!("Disconnecting for 5 seconds then reconnecting to Coinbase WS");
-                    break;
+                    continue;
                 }
+                Message::Error { message } => println!("Error: {}", message),
+                Message::InternalError(_) => panic!("internal_error"),
+                other => println!("{:?}", other),
+            };
+
+            let vwap = ladder.read().unwrap().vwap(3);
+            if vwap.is_nan() || vwap.is_infinite() || vwap <= 0.0 {
+                println!(
+                    "Price is invalid: {}, reconnecting as after 10 seconds",
+                    vwap
+                );
+                return;
             }
-            thread::sleep(std::time::Duration::from_secs(10));
+            match sender.send(vec![SDKMarketEvent::FairPriceUpdate { price: vwap }]) {
+                Ok(_) => {}
+                Err(e) => println!("Error while sending vwap update: {}", e),
+            }
         }
     }
 }
