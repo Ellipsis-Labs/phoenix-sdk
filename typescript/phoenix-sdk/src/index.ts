5e15bbe876dbe30135242efc563f69c4b172b4be
Arrowana <Arrowana@users.noreply.github.com>
2023-01-26 14:01:01 +0800

    feat: ladder using BN

--- typescript/phoenix-sdk/src/index.ts
+++ typescript/phoenix-sdk/src/index.ts
@@ -3,7 +3,7 @@ export * from "./errors";
 export * from "./instructions";
 export * from "./types";
 export * from "./market";
-export * from "./events";
+// export * from "./events"; issue with something assigned as bignum while not really being one
 
 /**
  * Program address
