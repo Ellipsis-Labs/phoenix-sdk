48cf9182585161756198f6b65c5775cc0ebaa2fa
James Li <sayhello.jamesli@gmail.com>
2023-04-06 11:54:35 -0400

    refactor tests to account for new amount in / amount out router functions

--- typescript/phoenix-sdk/test/amountOutAmountIn.ts
+++ typescript/phoenix-sdk/test/amountOutAmountIn.ts
@@ -1,7 +1,10 @@
 import { assert } from "chai";
 import { UiLadder } from "../src/market";
 import { Side } from "../src/types";
-import { getExpectedInAmountRouter } from "index";
+import {
+  getExpectedInAmountRouter,
+  getExpectedOutAmountRouter,
+} from "../src/utils/market";
 
 const mockLadder: UiLadder = {
   bids: [
@@ -16,31 +19,42 @@ const mockLadder: UiLadder = {
   ],
 };
 
-describe("Calculate expected amount out correctly for both bid and ask", () => {
-  it("Correctly calculate expected amount in given a desired amount out", () => {
-    // Test case where a user wants to receive 16 units of the base token and wants to know how many quote units they need to provide.
-    let side = Side.Bid;
-    const desiredBaseAmountOut = 16;
+describe("Calculate expected amount out and expected amount in for both bid and ask", () => {
+  it("Bid: Correctly calculate expected amount out given an amount in and an expected amount in given an amount out", () => {
+    const side = Side.Bid;
+    const quoteAmountIn = 25 * 10 + 30 * 5 + 35 * 1;
     const takerFeeBps = 5;
+    const expectedBaseAmountOut = getExpectedOutAmountRouter({
+      uiLadder: mockLadder,
+      takerFeeBps,
+      side,
+      inAmount: quoteAmountIn,
+    });
     const expectedQuoteAmountIn = getExpectedInAmountRouter({
       uiLadder: mockLadder,
       takerFeeBps,
       side,
-      outAmount: desiredBaseAmountOut,
+      outAmount: expectedBaseAmountOut,
     });
-    const quoteUnitsNeeded =
-      (25 * 10 + 30 * 5 + 35 * 1) / (1 - takerFeeBps / 10000);
-    assert.equal(expectedQuoteAmountIn, quoteUnitsNeeded);
-    // Test case where user wants to receive 285 units of quote token and wants to know how many base units they need to provide.
-    side = Side.Ask;
-    const desiredQuoteAmountOut = 20 * 10 + 15 * 5 + 10;
+    assert.equal(quoteAmountIn, expectedQuoteAmountIn);
+  });
+
+  it("Ask: Correctly calculate expected amount out given an amount in and an expected amount in given an amount out", () => {
+    const side = Side.Ask;
+    const baseAmountIn = 10 + 5 + 1;
+    const takerFeeBps = 5;
+    const expectedQuoteAmountOut = getExpectedOutAmountRouter({
+      uiLadder: mockLadder,
+      takerFeeBps,
+      side,
+      inAmount: baseAmountIn,
+    });
     const expectedBaseAmountIn = getExpectedInAmountRouter({
       uiLadder: mockLadder,
       takerFeeBps,
       side,
-      outAmount: desiredQuoteAmountOut,
+      outAmount: expectedQuoteAmountOut,
     });
-    const baseUnitsNeeded = (10 + 5 + 1) / (1 - takerFeeBps / 10000);
-    assert.equal(expectedBaseAmountIn, baseUnitsNeeded);
+    assert.equal(baseAmountIn, expectedBaseAmountIn);
   });
 });
