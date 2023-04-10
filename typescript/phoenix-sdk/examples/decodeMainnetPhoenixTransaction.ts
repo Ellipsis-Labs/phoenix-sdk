8ec7f1c95c082e8513bd5f3aa5aa816ac637d9b2
Jarry Xiao <jarry.xiao@gmail.com>
2023-04-10 17:14:57 -0400

    Added a comment to the decode function

--- typescript/phoenix-sdk/examples/decodeMainnetPhoenixTransaction.ts
+++ typescript/phoenix-sdk/examples/decodeMainnetPhoenixTransaction.ts
@@ -2,6 +2,52 @@ import { Connection } from "@solana/web3.js";
 
 import * as Phoenix from "../src";
 
+/*
+Prints out the Phoenix events for a specific transaction. Notable, the transaction being parsed in this
+example is a Solana V0 transaction (rather than a legacy Solana transaction)
+
+Expected output:
+[
+  {
+    "header": {
+      "instruction": 0,
+      "sequenceNumber": "065a8f",
+      "timestamp": "642b3343",
+      "slot": "0b1b9714",
+      "market": "FZRgpfpvicJ3p23DfmZuvUgcQZBHJsWScTf2N2jK8dy6",
+      "signer": "DqyMsuMucBVCqZ81Cw7H4hhs92Dt91oKB1wowKwxV2Fr",
+      "totalEvents": 2
+    },
+    "events": [
+      {
+        "__kind": "Fill",
+        "fields": [
+          {
+            "index": 0,
+            "makerId": "3HBWHuyxWv4uN8U8SeukocrWPfLZJqrtj9DgDHsGo2HR",
+            "orderSequenceNumber": "028501",
+            "priceInTicks": "01ae82",
+            "baseLotsFilled": "05587c",
+            "baseLotsRemaining": "01c30c"
+          }
+        ]
+      },
+      {
+        "__kind": "FillSummary",
+        "fields": [
+          {
+            "index": 1,
+            "clientOrderId": "00",
+            "totalBaseLotsFilled": "05587c",
+            "totalQuoteLotsFilled": "08fd922109",
+            "totalFeeInQuoteLots": "3aea11"
+          }
+        ]
+      }
+    ]
+  }
+]
+*/
 export async function decodeTransaction() {
   const connection = new Connection("https://api.mainnet-beta.solana.com");
   console.log(
