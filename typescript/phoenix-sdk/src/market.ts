4310baee31d880c5b56f87e3c5710d2fed9358a7
Jarry Xiao <jarry.xiao@gmail.com>
2023-05-10 17:35:44 -0400

    small update

--- typescript/phoenix-sdk/src/market.ts
+++ typescript/phoenix-sdk/src/market.ts
@@ -585,12 +585,12 @@ export class Market {
       exp5 += 1;
     }
     const precision = Math.max(exp2, exp5);
-    return (
-      Math.max(precision, 3) +
-      Math.floor(
-        Math.log10(Math.max(this.data.header.rawBaseUnitsPerBaseUnit, 1))
-      )
-    );
+    if (precision === 0) {
+      // In the off chance that the target does not have 2 or 5 as a prime factor,
+      // we'll just return a precision of 3 decimals.
+      return 3;
+    }
+    return precision;
   }
 
   /**
