8f3082afaaabaae9864a521ce14bf026c09f68d2
Jarry Xiao <jarry.xiao@gmail.com>
2023-07-03 16:52:01 -0500

    added custom commitments

--- typescript/phoenix-sdk/src/utils/connection.ts
+++ typescript/phoenix-sdk/src/utils/connection.ts
@@ -1,4 +1,9 @@
-import { Connection, PublicKey, SYSVAR_CLOCK_PUBKEY } from "@solana/web3.js";
+import {
+  Commitment,
+  Connection,
+  PublicKey,
+  SYSVAR_CLOCK_PUBKEY,
+} from "@solana/web3.js";
 import { ZSTDDecoder } from "zstddec";
 
 // Largest configured account size (multiplied by 2)
@@ -6,17 +11,20 @@ const MAX_ACCOUNT_SIZE_BYTES = 1723488 * 2;
 
 /**
  * Get market account data and clock data with zstd compression
- * @param marketKey The market address
+ * @param connection The web3 connection object
+ * @param marketKeys The market addresses
+ * @param commitment The commitment level
  * @returns
  */
 export const getConfirmedMarketsAndClockAccounts = async (
   connection: Connection,
-  marketKeys: PublicKey[]
+  marketKeys: PublicKey[],
+  commitment: Commitment = "confirmed"
 ): Promise<Buffer[]> => {
   const rawAccounts = (
     await connection.getMultipleAccountsInfo(
       [...marketKeys, SYSVAR_CLOCK_PUBKEY],
-      "confirmed"
+      commitment
     )
   ).map((account) => account?.data);
   if (!rawAccounts.every((account) => account !== undefined)) {
@@ -27,12 +35,15 @@ export const getConfirmedMarketsAndClockAccounts = async (
 
 /**
  * Get market account data and clock data with zstd compression
- * @param marketKey The market address
+ * @param connection The web3 connection object
+ * @param marketKeys The market addresses
+ * @param commitment The commitment level
  * @returns
  */
 export const getConfirmedMarketsAndClockAccountsZstd = async (
   connection: Connection,
-  marketKeys: PublicKey[]
+  marketKeys: PublicKey[],
+  commitment: Commitment = "confirmed"
 ): Promise<Buffer[]> => {
   const body = {
     jsonrpc: "2.0",
@@ -42,7 +53,7 @@ export const getConfirmedMarketsAndClockAccountsZstd = async (
       [...marketKeys.map((m) => m.toBase58()), SYSVAR_CLOCK_PUBKEY.toBase58()],
       {
         encoding: "base64+zstd",
-        commitment: "confirmed",
+        commitment,
       },
     ],
   };
@@ -81,12 +92,15 @@ export const getConfirmedMarketsAndClockAccountsZstd = async (
 
 /**
  * Get market account data with zstd compression
+ * @param connection The web3 connection object
  * @param marketKey The market address
+ * @param commitment The commitment level
  * @returns
  */
 export const getConfirmedMarketAccountZstd = async (
   connection: Connection,
-  marketKey: PublicKey
+  marketKey: PublicKey,
+  commitment: Commitment = "confirmed"
 ): Promise<Buffer> => {
   const body = {
     jsonrpc: "2.0",
@@ -96,7 +110,7 @@ export const getConfirmedMarketAccountZstd = async (
       marketKey.toBase58(),
       {
         encoding: "base64+zstd",
-        commitment: "confirmed",
+        commitment: commitment,
       },
     ],
   };
